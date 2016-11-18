44d00097f2ca142797ddb1f896671c2c85f7f088
Cadu Elmadjian <elmad@ime.usp.br>
2016-11-18 19:46:41 -0200

    adding growing contingency improvement

--- contour_code.py
+++ contour_code.py
@@ -4,6 +4,62 @@ import nibabel as nib
 import skimage
 import matplotlib.pyplot as plt
 
+# Get local adjacency
+#---------------------
+def get_neighborhood(neighborhood):
+    if neighborhood == 4:
+        return [(-1,0), (0,1), (1,0), (0,-1)]
+    if neighborhood == 6:
+        return [(-1,0,0),(0,1,0),(1,0,0),(0,-1,0),(0,0,-1),(0,0,1)]
+    if neighborhood == 8:
+        return [(-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1)]
+    if neighborhood == 24:
+        return [(-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1),
+                (-2,-2), (-2,-1), (-2,0), (-2,1), (-2,2), (-1,2), (0,2), (1,2),
+                (2,2), (2,1), (2,0), (2,-1), (2,-2), (1,-2), (0,-2), (-1,-2)]
+
+
+#Initialize all pixels with a specific cost rule
+#------------------------------------------------
+def initialize_costs(img, neighborhood):
+    cost = {}
+    img_copy = img.copy()
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            img_copy[(y,x)] = 255
+            cost[(y,x)] = calc_cost(img, (y,x), neighborhood)
+            cv2.imshow("teste", img_copy)
+            cv2.waitKey(10)
+    return cost
+
+#Check whether the element is inside the image/volume
+#----------------------------------------------------
+def is_valid_pixel(img, pixel):
+    y = pixel[0]
+    x = pixel[1]
+    if len(img.shape) == 3:
+        z = pixel[2]
+    if y >= 0 and y < img.shape[0]:
+        if x >= 0 and x < img.shape[1]:
+            if len(img.shape) == 2:
+                return True
+            else:
+                if z >= 0 and z < img.shape[2]:
+                    return True
+    return False
+
+#Calculates the cost at a specific pixel/voxel
+#---------------------------------------------
+def calc_cost(img, point, neighborhood):
+    points = [(point[0] + i[0], point[1] + i[1]) for i in neighborhood]
+    I = []
+    for p in points:
+        if is_valid_pixel(img, p):
+            I.append(img[p])
+    mean = np.mean(I)
+    stdd = np.std(I)
+    print(mean, stdd)
+    #return mean + 1.0/stdd
 
 
 def main():
@@ -12,24 +68,28 @@ def main():
     cv2.imshow("teste", thresh)
     cv2.waitKey(0)
 
-    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[1]
-    tam = len(contours[0])
-    for i in range(tam):
-        xi = contours[0][i][0][0]
-        yi = contours[0][i][0][1]
-        pixel_sum = 0
-        for j in range(10):
-            xjA = contours[0][(i+j)%tam][0][0]
-            yjA = contours[0][(i+j)%tam][0][1]
-            xjB = contours[0][i-j][0][0]
-            yjB = contours[0][i-j][0][1]
-            pixel_sum += img[yjA, xjA] + img[yjB, xjB]
-        print(pixel_sum)
+    # contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[1]
+    # tam = len(contours[0])
+    # for i in range(tam):
+    #     xi = contours[0][i][0][0]
+    #     yi = contours[0][i][0][1]
+    #     pixel_sum = 0
+    #     for j in range(10):
+    #         xjA = contours[0][(i+j)%tam][0][0]
+    #         yjA = contours[0][(i+j)%tam][0][1]
+    #         xjB = contours[0][i-j][0][0]
+    #         yjB = contours[0][i-j][0][1]
+    #         pixel_sum += img[yjA, xjA] + img[yjB, xjB]
+    #     print(pixel_sum)
+
+    neighborhood = get_neighborhood(8)
+    initialize_costs(img, neighborhood)
 
 
 
-    cv2.imshow("teste", thresh)
-    cv2.waitKey(0)
+
+    # cv2.imshow("teste", thresh)
+    # cv2.waitKey(0)
 
 
 
--- skull_ift.py
+++ skull_ift.py
@@ -16,9 +16,13 @@ def get_neighborhood(neighborhood):
         return [(-1,0,0),(0,1,0),(1,0,0),(0,-1,0),(0,0,-1),(0,0,1)]
     if neighborhood == 8:
         return [(-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1)]
+    if neighborhood == 24:
+        return [(-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1),
+                (-2,-2), (-2,-1), (-2,0), (-2,1), (-2,2), (-1,2), (0,2), (1,2),
+                (2,2), (2,1), (2,0), (2,-1), (2,-2), (1,-2), (0,-2), (-1,-2)]
 
-
-#-------------------------
+#Check whether the element is inside the image/volume
+#----------------------------------------------------
 def is_valid_pixel(img, pixel):
     y = pixel[0]
     x = pixel[1]
@@ -33,32 +37,44 @@ def is_valid_pixel(img, pixel):
                     return True
     return False
 
-#Image-Forest Transform
-#---------------------------------
+#Image-Foresting Transform
+#--------------------------
 def ift(img, current, neighborhood, conquest, Q, cost_dic, predecessor):
     for i in neighborhood:
         pixel = (current[0] + i[0], current[1] + i[1])
         if not is_valid_pixel(img, pixel):
             continue
         if conquest[pixel] != 255:
-            cost = cost_dic[current] + ((int(img[current]) + int(img[pixel]))/2)
-            if cost < cost_dic[pixel]:
-                cost_dic[pixel] = cost
+            #cost = cost_dic[current] + ((int(img[current]) + int(img[pixel]))/2)
+            #if cost < cost_dic[pixel]:
+            if cost_dic[pixel] >= 0 and img[pixel] > 30:
+                #cost_dic[pixel] = cost
                 predecessor[pixel] = current
-                Q.put(pixel, cost)
+                Q.put(pixel, cost_dic[pixel])
 
-#Initialize all pixels with cost infinity
-#----------------------------------------
-def initialize_costs(img):
+#Initialize all pixels with a specific cost rule
+#------------------------------------------------
+def initialize_costs(img, neighborhood):
     cost = {}
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
-            #COLOCAR 3D aqui
-            if img[(y,x)] < 30:
-                cost[(y,x)] = 0
-            else:
-                cost[(y,x)] = sys.maxsize
+            cost[(y,x)] = calc_cost(img, (y,x), neighborhood)
     return cost
+
+#Calculates the cost at a specific pixel/voxel
+#---------------------------------------------
+def calc_cost(img, point, neighborhood):
+    if img[point] == 0:
+        return 0
+    points = [(point[0] + i[0], point[1] + i[1]) for i in neighborhood]
+    I = []
+    for p in points:
+        if is_valid_pixel(img, p):
+            I.append(img[p])
+    mean = np.mean(I)
+    stdd = np.std(I)
+    return 1/(3 * stdd + 1) - 1/(mean + 1)
+
 
 
 def main():
@@ -70,28 +86,32 @@ def main():
     conquest = np.zeros(norm_img.shape, dtype="uint8")
 
     #extract slice from an axis
-    #for i in range(20, 220):
-    i=121
-    slc = norm_img[:, i, :]
-    slcc = conquest[:, i, :]
-    cv2.imshow("teste", slc)
-    cv2.imwrite("slice.png", slc)
-    cv2.waitKey(0)
+    for i in range(80, 220):
+        #i=181
+        slc = norm_img[:, i, :]
+        slcc = conquest[:, i, :]
+        cv2.imshow("teste", slc)
+        cv2.imwrite("slice.png", slc)
+        cv2.waitKey(0)
 
-    Q = graph.PriorityQueue()
-    n = (144,56)
-    Q.put(n, 0)
-    neighborhood = get_neighborhood(4)
-    predecessor = {}
-    cost = initialize_costs(slc)
-    cost[n] = 0
+        Q = graph.PriorityQueue()
+        n = (144,56)
+        Q.put(n, 0)
+        neighborhood = get_neighborhood(4)
+        window = get_neighborhood(24)
+        predecessor = {}
+        cost = initialize_costs(slc, window)
+        cost[n] = 1
 
-    while not Q.is_empty():
-        lowest = Q.pop()
-        slcc[lowest] = 255
-        ift(slc, lowest, neighborhood, slcc, Q, cost, predecessor)
+        while not Q.is_empty():
+            lowest = Q.pop()
+            slcc[lowest] = 255
+            ift(slc, lowest, neighborhood, slcc, Q, cost, predecessor)
+            # cv2.imshow("teste2", slcc)
+            # cv2.waitKey(1)
+
         cv2.imshow("teste2", slcc)
-        cv2.waitKey(1)
+        cv2.waitKey(0)
 
 
     print("finished")
