28e69f2c2abce81307406f9c31bf7d68a5f37b49
elmadjian <cadu_elmadjian@yahoo.com.br>
2016-12-07 02:33:53 -0200

    adding an algorithm for initial seed

--- skull_edges.py
+++ skull_edges.py
@@ -6,31 +6,105 @@ import matplotlib.pyplot as plt
 
 def main():
     #load data
-    img3D    = nib.load("../1.nii.gz")
+    img3D    = nib.load("../6.nii.gz")
     img_data = img3D.get_data()
     max_val  = img_data[:,:,:].max()
     norm_img = np.uint8(img_data[:,:,:]*255.0/max_val)
 
-    #extract slice from an axis
-    for i in range(100, 220):
-        #i = 140
-        slc = norm_img[:, :, i]
+    print(norm_img.shape)
 
-        cv2.imshow("teste", slc)
-        cv2.waitKey(0)
+    y = norm_img.shape[0]
+    x = norm_img.shape[1]
+    z = norm_img.shape[2]
+    yd = [[0, y//3], [y//3, (y//3)*2], [(y//3)*2, y]]
+    xd = [[0, x//3], [x//3, (x//3)*2], [(x//3)*2, x]]
+    zd = [[0, z//3], [z//3, (z//3)*2], [(z//3)*2, z]]
 
-        half = slc.shape[0]//2
-        x_list = []
-        y_list = []
-        for i in range(slc.shape[1]):
-            x_list.append(i)
-            y_list.append(slc[half][i])
-            slc[half][i] = 255
+    cubes = []
+    coord = []
+    for iy in yd:
+        for ix in xd:
+            for iz in zd:
+                coords = [iy, ix, iz]
+                cubes.append(norm_img[iy[0]:iy[1], ix[0]:ix[1], iz[0]:iz[1]])
+                coord.append(coords)
 
-        # cv2.imshow("teste", slc)
-        # cv2.waitKey(0)
-        # plt.plot(x_list, y_list)
-        # plt.show()
+    argmax = -1
+    highest_mean = -1
+    for i in range(len(cubes)):
+        mean = np.mean(cubes[i]))
+        if mean > highest_mean:
+            highest_mean = mean
+            argmax = (y,x,z)
+        y = coord[i][0][0] + (coord[i][0][1]-coord[i][0][0])//1.5
+        x = coord[i][1][0] + (coord[i][1][1]-coord[i][1][0])//1.5
+        z = coord[i][2][0] + (coord[i][2][1]-coord[i][2][0])//1.5
+
+
+        # for i in range(cube.shape[1]):
+        #
+        #     slc = cube[:,i,:]
+        #     cv2.imshow("teste", slc)
+        #     cv2.waitKey(0)
+
+    # maxpeak = -1
+    # minpeak = sys.maxsize
+    # interval = [0 for i in range(10)]
+    # prev = 0
+    # count = 0
+    #
+    # maxpoint = {}
+
+    # #extract slice from an axis
+    # for i in range(0, 240):
+    #     #i = 140
+    #     slc = norm_img[:, i, :]
+    #
+    #     cv2.imshow("teste", slc)
+    #     cv2.waitKey(0)
+    #
+    #     # diff = abs(np.mean(slc) - np.std(slc))
+    #     # interval.pop(0)
+    #     # interval.append(diff)
+    #     # top = np.max(interval)
+    #     # if top > maxpeak:
+    #     #     maxpeak = top
+    #     # elif top < minpeak:
+    #     #     minpeak = top
+    #     # if top > prev:
+    #     #     count += 1
+    #     #     print("crescendo---->", count)
+    #     # if top < prev:
+    #     #     count -= 1
+    #     #     print("descendo<----", count)
+    #     # prev = top
+    #     #
+    #     #
+    #     # print("max:", maxpeak, "min:", minpeak, "diff:", maxpeak-minpeak)
+    #     #
+    #     # print("calc:", top)
+    #     point = np.unravel_index(slc.argmax(), slc.shape)
+    #     if point not in maxpoint.keys():
+    #         maxpoint[point] = 0
+    #     maxpoint[point] += 1
+    #
+    #     print("argmax:", np.unravel_index(slc.argmax(), slc.shape))
+    #     print("---------------------")
+    #
+    #     half = slc.shape[0]//2
+    #     x_list = []
+    #     y_list = []
+    #     for i in range(slc.shape[1]):
+    #         x_list.append(i)
+    #         y_list.append(slc[half][i])
+    #         slc[half][i] = 255
+    #
+    #     # cv2.imshow("teste", slc)
+    #     # cv2.waitKey(0)
+    #     # plt.plot(x_list, y_list)
+    #     # plt.show()
+    # for k in maxpoint.keys():
+    #     print(k, maxpoint[k])
 
 
     # ret, thresh = cv2.threshold(slc, 0, 255, cv2.THRESH_OTSU)
--- skull_ift_3d.py
+++ skull_ift_3d.py
@@ -61,7 +61,6 @@ def ift_fg(img, current, neighborhood, conquest, Q, cost_dic):#, predecessor):
             cost = 255 * np.exp(-int(img[pixel])/20.0)
             if cost < cost_dic[pixel]:
                 cost_dic[pixel] = cost
-                #predecessor[pixel] = current
                 Q.put(pixel, cost)
 
 #Initialize all pixels with a specific cost rule
@@ -93,66 +92,83 @@ def calc_cost(img, point, neighborhood):
 #----------------------------------------------
 def post_process(img, thresh):
     maximum = -1
-    for i in range(img.shape[1]-1, -1, -1):
+    for i in range(img.shape[1]-1, -1, -2):
         slc = img[:, i, :]
         mean = np.mean(slc)
         if mean >= maximum:
             maximum = mean
         else:
-            if np.mean(slc) < thresh:
-                img[:, i:rebuilt.shape[1], :] = 0
+            if mean < thresh:
+                img[:, 0:i, :] = 0
                 break
 
+# Find starting point
+#--------------------
+def get_init_point(img):
+    y = img.shape[0]
+    x = img.shape[1]
+    z = img.shape[2]
+    yd = [[0, y//3], [y//3, (y//3)*2], [(y//3)*2, y]]
+    xd = [[0, x//3], [x//3, (x//3)*2], [(x//3)*2, x]]
+    zd = [[0, z//3], [z//3, (z//3)*2], [(z//3)*2, z]]
+    cubes, coord = [], []
+    for iy in yd:
+        for ix in xd:
+            for iz in zd:
+                coords = [iy, ix, iz]
+                cubes.append(img[iy[0]:iy[1], ix[0]:ix[1], iz[0]:iz[1]])
+                coord.append(coords)
+    argmax, highest_mean = -1, -1
+    for i in range(len(cubes)):
+        mean = np.mean(cubes[i])
+        if mean > highest_mean:
+            highest_mean = mean
+            argmax = (y,x,z)
+        y = int(coord[i][0][0] + (coord[i][0][1]-coord[i][0][0])/1.6)
+        x = int(coord[i][1][0] + (coord[i][1][1]-coord[i][1][0])/1.6)
+        z = int(coord[i][2][0] + (coord[i][2][1]-coord[i][2][0])/1.6)
+    return argmax
 
+
+#skull strip program
+#-------------------
 def main():
     #load data
-    img3D    = nib.load("../1.nii.gz")
+    img3D    = nib.load("../6.nii.gz")
     img_data = img3D.get_data()
     max_val  = img_data[:,:,:].max()
     norm_img = np.uint8(img_data[:,:,:]*255.0/max_val)
     conquest = np.zeros(norm_img.shape, dtype="uint8")
 
     #grow ift 3D
-    Q_bg = graph.PriorityQueue()
     Q_fg = graph.PriorityQueue()
-    n_bg = (0,0,0)
-    n_fg = (150,144,56)
-    Q_bg.put(n_bg, 0)
+    n_fg = get_init_point(norm_img)
     Q_fg.put(n_fg, 0)
     neighborhood = get_neighborhood(6)
-    predecessor = {}
-
-    print("initializing costs...")
-    #cost = initialize_costs(norm_img)
-    cost = norm_img.astype("float32")
-    cost = np.log(cost/20.0 + 1) * 25
 
     print("applying 3D erosion...")
     norm_img = morphology.erosion(norm_img, morphology.ball(2))
 
-    # while not Q_bg.is_empty():
-    #     lowest = Q_bg.pop()
-    #     conquest[lowest] = 255
-    #     ift_bg(norm_img, lowest, neighborhood, conquest, Q_bg, cost)#, predecessor)
-
+    print("initializing costs...")
+    cost = norm_img.astype("float32")
+    cost = np.log(cost/20.0 + 1) * 30
     cost[n_fg] = 1
 
     print("processing foreground...")
     while not Q_fg.is_empty():
         lowest = Q_fg.pop()
         conquest[lowest] = 255
-        ift_fg(norm_img, lowest, neighborhood, conquest, Q_fg, cost)#, predecessor)
+        ift_fg(norm_img, lowest, neighborhood, conquest, Q_fg, cost)
 
     print("applying post-processing...")
-    rebuilt = morphology.dilation(conquest, morphology.ball(5))
-    post_process(rebuilt, 33)
+    rebuilt = morphology.dilation(conquest, morphology.ball(4))
+    post_process(rebuilt, 6)
 
     print("saving label...")
     new_img = nib.Nifti1Image(rebuilt, np.eye(4))
-    nib.save(new_img, "test_label2.nii.gz")
-
-
-
+    processed = nib.Nifti1Image(img_data, np.eye(4))
+    nib.save(processed, "test_6.nii.gz")
+    nib.save(new_img, "test_label_6.nii.gz")
 
 
 
